896905653c99cb60284776bd9407b83017d6d095
Justin Hong <jhong2009@gmail.com>
2018-03-29 12:56:11 -0700

    selected tabs item via their data attributes and revised select/deslect()

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,26 +1,30 @@
 
+// TabsItem refers to the tab contents only
 class TabsItem {
   constructor(element) {
     // attach dom element to object. Example in Tabs class
     this.element = element;
+    this.dataTab = element.dataset.tab;
+    this.tabsItem = document.querySelector(`.Tabs__item[data-tab="${this.dataTab}"]`);
   }
 
   select() {
     // should use classList
-    this.Tabs__item.classList.toggle('Tabs__item-selected');
+    this.tabsItem.classList.toggle('Tabs__item-selected');
   }
 
   deselect() {
     // should use classList
-    this.Tabs__item.classList.toggle('Tabs__item')
+    this.tabsItem.classList.toggle('Tabs__item');
   }
 }
 
+// TabsLink connects a link to its corresponding item
 class TabsLink {
   constructor(element, parent) {
     this.element = element;// attach dom element to object
     this.tabs = parent;// attach parent to object
-    this.tabsItem = this.Tabs.getTab(element.dataset.data-tab);// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
+    this.tabsItem = this.Tabs.getTab(element.dataset.tab);// assign this to the associated tab using the parent's "getTab" method by passing it the correct data
     // reassign this.tabsItem to be a new instance of TabsItem, passing it this.tabsItem
     this.tabsItem = new TabsItem(this.tabsItem);
     this.element.addEventListener('click', () => {
@@ -31,12 +35,16 @@ class TabsLink {
 
   select() {
     // select this link
+
     // select the associated tab
+
   }
 
   deselect() {
     // deselect this link
+
     // deselect the associated tab
+
   }
 }
 
@@ -68,3 +76,5 @@ class Tabs {
 
 let tabs = document.querySelectorAll(".Tabs");
 tabs = Array.from(tabs).map(tabs => new Tabs(tabs));
+
+
